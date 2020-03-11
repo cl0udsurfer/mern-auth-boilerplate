c4828ef1acaa83e51745619d82784ae1d74c0de6
cl0udsurfer <datakobs1@gmail.com>
2020-03-11 12:45:44 +0100

    api account activation signup and signin

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup } = require('../controllers/auth');
+const { signup, accountActivation, signin } = require('../controllers/auth');
 
-router.get('/auth/signup', signup);
+const {
+  userSignupValidator,
+  userSigninValidator
+} = require('../validators/auth');
+const { runValidation } = require('../validators');
+
+router.post('/auth/signup', userSignupValidator, runValidation, signup);
+router.post('/auth/account-activation', accountActivation);
+router.post('/auth/signin', userSigninValidator, runValidation, signin);
 
 module.exports = router;
